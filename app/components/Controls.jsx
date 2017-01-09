1c2eae3b6a5ef6597b52ebede874f465c823e3e6
Don Ayers <donpaulayers@gmail.com>
2017-01-08 20:19:43 -0800

    wired up timer

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var Controls = React.createClass({
 	propTypes: {
-		countdownStatus: React.PropTypes.string.isRequired,
+		//countdownStatus: React.PropTypes.string.isRequired,
 		onStatusChange: React.PropTypes.func.isRequired
 	},
 	onStatusChange: function(newStatus){
@@ -11,12 +11,12 @@ var Controls = React.createClass({
 		}
 	},
 	render: function(){
-		var {countdownStatus} = this.props;
+		var {countdownStatus, timerStatus} = this.props;
 
 		var renderStartStopButton = () => {
-			if(countdownStatus === 'started'){
+			if(countdownStatus === 'started' || timerStatus === 'started'){
 				return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
-			}else if (countdownStatus === 'paused') {
+			}else if (countdownStatus === 'paused' || timerStatus === 'stopped' || timerStatus === 'paused') {
 				return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
 			}
 		};
